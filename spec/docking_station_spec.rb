c48981bedafc486b1aaff42caf36a4716e3357e8
Shereen Finney <shereenfinney@gmail.com>
2017-01-07 20:01:28 +0000

    Added a test to spec file that expects DockingStation instances to respond_to the method release_bike - using implicitly defined subject syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,14 @@
+# In /Users/shereen/Google Drive/Projects/boris-bikes/spec/docking_station+spec.rb
 require "docking_station"
 
-# In /Users/shereen/Google Drive/Projects/boris-bikes/spec/docking_station+spec.rb
-
 describe DockingStation do
+
+# 1. Add a test to your spec file that expects DockingStation instances to respond_to the method release_bike
+
+#implicitly defined subject syntax
+it 'releases a bike' do
+  expect(subject).to respond_to :release_bike # ds is the instance of the DockingStation object and :release_bike will be the class's method
+
+end
 
 end
