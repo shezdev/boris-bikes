5f9d8106f5e737daddae12f3ec59f7c0f8278f9d
Shereen Finney <shereenfinney@gmail.com>
2017-01-07 20:05:54 +0000

    Re-written test using RSpec 1 liner syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,9 +6,13 @@ describe DockingStation do
 # 1. Add a test to your spec file that expects DockingStation instances to respond_to the method release_bike
 
 #implicitly defined subject syntax
-it 'releases a bike' do
-  expect(subject).to respond_to :release_bike # ds is the instance of the DockingStation object and :release_bike will be the class's method
+# it 'releases a bike' do
+#   expect(subject).to respond_to :release_bike # ds is the instance of the DockingStation object and :release_bike will be the class's method
+#end
 
-end
+#2. Rewritten using Rspec one liner syntax
+it { is_expected.to respond_to :release_bike }
+
+
 
 end
